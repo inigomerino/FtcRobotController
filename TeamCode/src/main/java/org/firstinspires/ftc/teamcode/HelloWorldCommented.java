8c1b4885446639fb60e6c59fbf27012fd9414d5a
Inigo Merino <inigo@cienagasystems.net>
2023-10-15 01:04:03 -0400

    Simple test
    To ensure the project works -- control the robot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorldCommented.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//@TeleOp(name="HelloWorld", group="Sample")
+@TeleOp
+public class HelloWorldCommented extends OpMode {
+    /**
+     * This is called when the driver presses INIT */
+    @Override
+    public void init() {
+        // this sends to the driver station
+        telemetry.addData("Hello","Init-1");
+    }
+    /**
+     * This is called repeatedly while OpMode is playing */
+    @Override
+    public void loop() {
+        // intentionally left blank
+        telemetry.addData("Hello","Hello World Loop");
+    }
+}
